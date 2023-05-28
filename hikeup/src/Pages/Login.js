81a809794a9baa19ad6d44b2b02d50002a83e9fc
Dorde <aleks.dorde@gmail.com>
2023-05-28 05:52:50 +0200

    d

---
+++ hikeup/src/Pages/Login.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import '../Styles/login.css'; // Import CSS file for styling
+import { useNavigate } from 'react-router-dom';
+
+const Login = () => {
+  const navigate = useNavigate()
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    if(username === 'ivan' && password === '1122'){
+      navigate('/')
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <form className="login-form" onSubmit={handleLogin}>
+        <h2>Login</h2>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login
